bfd5904a4dee12fcd350dd1ce299f532b4ee362a
sassplus <huangzheng326@gmail.com>
2015-11-24 00:19:19 +0800

    add ajax-demo

--- ajax-demo/ajax-AJAX/test.js
+++ ajax-demo/ajax-AJAX/test.js
@@ -18,4 +18,4 @@ var comments = [
       html += '<div class="comment"><h6>' + comment['username'] + ':</h6><p class="para">' + comment['content'] + '</p></div>';
   })
 
-  $('#resText').html(html);
+  $('#restext').html(html);
